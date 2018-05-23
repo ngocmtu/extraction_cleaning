4d84bef9c8d7c3b644a52f1b480b391213b9825e
Ngoc Tu <ngocbeo196@gmail.com>
2018-05-23 10:24:56 -0500

    First commit with extract.py that changes name and extract information for an entire directory, extract_one_file.py does all that but with a single file

---
+++ extract_one_file.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import pandas as pd
+from os import listdir
+from os import rename
+from os import path
+from os import remove
+from os import makedirs
+
+f = sys.argv[1] # full path to input file
+
+# method to find position of certain characters
+def findnth(haystack, needle, n):
+	n = n-1
+	parts = haystack.split(needle, n+1)
+	if len(parts)<=n+1:
+		return -1
+	return len(haystack)-len(parts[-1])-len(needle)
+
+def shorten_name(original_name):
+	#find position of year
+	#then get year to a var called year
+	#if file name's already changed, then do nothing
+	path_name, original_name = path.split(original_name)
+	yeardelim = findnth(original_name,'2F',1)
+
+	if yeardelim == -1:
+		return path.join(path_name,original_name)
+	else:
+		year = original_name[yeardelim+2:yeardelim+6]
+		print('Year '+year)
+
+		# get positions of month in name
+		# usually between the last two '_' characters out of three '_' characters
+		# if there's only two, count the length of name after the last '_'
+		# if 17, get the first number after '_' as month
+		# if 18, get the two numbers after '_' as month
+		firstdelim = findnth(original_name,'_',2)
+		seconddelim = findnth(original_name,'_',3)
+
+		if seconddelim == -1:
+			if len(original_name[firstdelim:-1]) == 17:
+				month = original_name[firstdelim+1]
+			else:
+				month = original_name[firstdelim+1:firstdelim+3]
+		else:
+			month = original_name[firstdelim+1:seconddelim]
+
+		new_name = year+'_'+month+'_'+original_name[-5]+'.csv'
+		rename(current_dir+original_name,current_dir+new_name)
+		return path.join(path_name,new_name)
+
+# if name already shorten, return the same name
+# if not, rename it to make it shorter
+print('Before '+f)
+fr = shorten_name(f)
+print('After '+f)
+
+# put file into a DataFrame for further processing
+df = pd.read_csv(f)
+
+# get inputfile name without .csv extension
+input_name = f[0:-4]
+
+# create name for the new output files
+fwbull = input_name + '_bull.csv'
+fwbear = input_name + '_bear.csv'
+fwnosen = input_name + '_nosen.csv'
+
+# read file and write to corresponding bull or bear file
+# if no sentiment attached, write to a file that's empty
+with open(fwbull,'w+') as write_bull, open(fwbear, 'w+') as write_bear, open(fwnosen, 'w+') as write_no_sentiment:
+	# create csvwriter to write in csv format to output files
+	csvwrite_bull = csv.writer(write_bull,dialect='excel')
+	csvwrite_bear = csv.writer(write_bear,dialect='excel')
+	csvwrite_no_sentiment = csv.writer(write_no_sentiment,dialect='excel')
+
+	# write column names
+	# no_sentiment file does not have sentiment so it does not have that column
+	csvwrite_bull.writerow(['created_at','tweet', 'sentiment'])
+	csvwrite_bear.writerow(['created_at','tweet', 'sentiment'])
+	csvwrite_no_sentiment.writerow(['created_at','tweet'])
+
+	# for every row in the original file
+	# write to each of the corresponding 3 files
+	for i in range(len(df)):
+		row = df.loc[i,:]
+		sen = row['entities_sentiment_basic']
+		tweet = row['object_summary']
+		time = row['object_postedTime']
+		if sen == 'Bullish':
+			csvwrite_bull.writerow([time,tweet,sen])
+		elif sen == 'Bearish':
+			csvwrite_bear.writerow([time,tweet,sen])
+		else:
+			csvwrite_no_sentiment.writerow([time,tweet])
+
+# remove original file after it has served its purpose
+# remove(d+fr)
+	
